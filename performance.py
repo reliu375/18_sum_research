7f936b8534e1bb583b14e77ced99443c89264ec6
Renbin Liu <reliu375@gmail.com>
2018-07-23 11:44:22 -0400

    print testing results

--- performance.py
+++ performance.py
@@ -1,4 +1,12 @@
 import focaltrack
 import cProfile
+import pstats
+from pstats import SortKey
 
+pr = cProfile.Profile()
+pr.enable()
 cProfile.run(focaltrack.multithreading_test())
+pr.disable()
+sortby = SortKey.CUMULATIVE
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
