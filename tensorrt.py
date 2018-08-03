085f2352ba4d4955638f7b97185441818276f575
Renbin Liu <reliu375@gmail.com>
2018-08-03 16:03:02 -0400

    tensorrt code

--- tensorrt.py
+++ tensorrt.py
@@ -22,22 +22,17 @@ session.run(init)
 graphdef = tf.get_default_graph().as_graph_def()
 frozen_graph = tf.graph_util.convert_variables_to_constants(sess, graphdef, OUTPUT)
 
+model = tf.graph_util.remove_training_nodes(frozen_graph)
 
-	
-'''
-import tensorrt as trt
-from tensorrt.parsers import uffparser
-
-uff_model = uff.from_tensorflow(init_op)
+uff_model = uff.from_tensorflow(model, ["add"])
 
 G_LOGGER = trt.infer.ConsoleLogger(trt.infer.LogSeverity.ERROR)
 
 parser = uffparser.create_uff_parser()
-parser.register_input(x, (2, 2), 0)
-parser.register_output(y)
+parser.register_output("add")
 
-engine = trt.utils.uff_to_trt_engine(G_LOGGER, uff_model, parser)
-
+engine = trt.utils.uff_to_trt_engine(G_LOGGER, uff_model, parser, ) 
+# TODO: fill in the rest of the arguments
 parser.destroy()
 
 runtime = trt.infer.create_infer_runtime(G_LOGGER)
@@ -45,20 +40,14 @@ context = engine.create_execution_context()
 
 output = np.empty(10, dtype = np.float32)
 
-d_input = cuda.mem_alloc(x.nbytes)
-d_output = cuda.mem_alloc(output.nbytes)
+d_input = 0
+d_output = cuda.mem_alloc(1 * output.nbytes)
 
 bindings = [int(d_input), int(d_output)]
 
 stream = cuda.Stream()
 
 context.enquene(1, bindings, stream.handle, None)
-# This is to be continued. 
-'''
 
-''' An alternative of parsing a TensorFlow graph.
-
-
-
-'''
-
+stream.synchronize()
+	
